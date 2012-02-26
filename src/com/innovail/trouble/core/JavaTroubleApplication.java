5906b21cf1300b26c5a64caaecf8155c11fc596a
Bernd Michael Weber <berndmicweber@innovail.com>
2012-02-26 15:16:41 -0800

    Adding application code.

---
+++ src/com/innovail/trouble/core/JavaTroubleApplication.java
@@ -0,0 +1,69 @@
+/**
+ * @file:   com.innovail.trouble.core - JavaTroubleApplication.java
+ * @date:   Feb 26, 2012
+ * @author: bweber
+ */
+package com.innovail.trouble.core;
+
+import com.badlogic.gdx.ApplicationListener;
+
+/**
+ * 
+ */
+public class JavaTroubleApplication implements ApplicationListener {
+
+    /* (non-Javadoc)
+     * @see com.badlogic.gdx.ApplicationListener#create()
+     */
+    @Override
+    public void create () {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see com.badlogic.gdx.ApplicationListener#resize(int, int)
+     */
+    @Override
+    public void resize (int width, int height) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see com.badlogic.gdx.ApplicationListener#render()
+     */
+    @Override
+    public void render () {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see com.badlogic.gdx.ApplicationListener#pause()
+     */
+    @Override
+    public void pause () {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see com.badlogic.gdx.ApplicationListener#resume()
+     */
+    @Override
+    public void resume () {
+        // TODO Auto-generated method stub
+
+    }
+
+    /* (non-Javadoc)
+     * @see com.badlogic.gdx.ApplicationListener#dispose()
+     */
+    @Override
+    public void dispose () {
+        // TODO Auto-generated method stub
+
+    }
+
+}
